cc1f306ff2bef95ea0eb14eb6bb59630dacb6210
gilbertlc <gilbert.lacasandile@gmail.com>
2022-03-05 13:18:18 +0800

    done testimonial slider

--- js/main.js
+++ js/main.js
@@ -1,97 +1,109 @@
-const carousel = ({prevBtn, nextBtn, container, items, interval}) => {
-  let size = 0;
+const slider = () => {
+  const track = document.querySelector('.testimonials__carousel-container');
+  const container = document.querySelector('.testimonials__container');
+  const nextBtn = document.querySelector('.testimonials__next-btn');
+  const prevBtn = document.querySelector('.testimonials__prev-btn');
   let counter = 0;
-  let gap = parseFloat(getComputedStyle(container).gap);
+  const gap = window.getComputedStyle(track).getPropertyValue('gap');
+  const slides = document.querySelectorAll('.testimonials__text');
+  const size = slides[0].offsetWidth + parseInt(gap);
+
+  // gesture states
+  let initPos = 0;
+  let drag = false;
+  let transformed = 0;
 
   const apply = () => {
-    container.style.transition = "0.4s";
-    container.style.transform = `translateX(${-size}px)`;
+    track.style.transition = '.3s';
+    track.style.transform = `translateX(-${size * counter}px)`;
   };
 
-  const handleNextBtn = () => {
-    if (counter + 1 === items.length) {
-      size = 0;
-      counter = 0;
-    } else {
-      size = size + items[counter].clientWidth + gap;
-      counter++;
-    }
+  const increaseCounter = () => {
+    counter = counter >= slides.length - 1 ? 0 : counter + 1;
+  };
+
+  const decreaseCounter = () => {
+    counter = counter <= 0 ? slides.length - 1 : counter - 1;
+  };
+
+  const slideNext = () => {
+    increaseCounter();
+    apply();
+  };
+
+  const slidePrev = () => {
+    decreaseCounter();
+    apply();
+  };
+
+  const getTransformed = () => {
+    const matrix = window.getComputedStyle(track).getPropertyValue('transform');
+    return matrix !== 'none' ? parseInt(matrix.split(',')[4]) : 0;
+  };
+
+  const gestureStart = (e) => {
+    drag = true;
+    initPos = e.pageX;
+
+    transformed = getTransformed();
+  };
+
+  const gestureMove = (e) => {
+    if (!drag) return;
+
+    const currentPos = e.pageX;
+    const lastCounter = slides.length - 1;
+    const endPos = size * lastCounter;
+    let posX = transformed + currentPos - initPos;
+
+    if (Math.abs(posX) > endPos) posX = -endPos;
+    else if (posX > 0) posX = 0;
+
+    track.style.transition = 'none';
+    track.style.transform = `translateX(${posX}px)`;
+  };
+
+  const gestureEnd = () => {
+    drag = false;
+
+    const moved = getTransformed() - transformed;
+
+    if (moved < -100 && counter < slides.length - 1) counter += 1;
+    if (moved > 100 && counter > 0) counter -= 1;
 
     apply();
   };
 
-  const handlePrevBtn = () => {
-    if (counter === 0) {
-      size = 0;
+  nextBtn.addEventListener('click', slideNext);
+  prevBtn.addEventListener('click', slidePrev);
 
-      items.forEach((image) => {
-        size = size + image.clientWidth + gap;
-      });
-
-      size = size - (items[items.length - 1].clientWidth + gap);
-
-      counter = items.length - 1;
-    } else {
-      counter--;
-      size = size - items[counter].clientWidth - gap;
-    }
-
-    apply();
-  };
-
-  let timer;
-
-  if (interval) timer = setInterval(handleNextBtn, interval);
-
-  const resetTimer = () => {
-    clearInterval(timer);
-    timer = setInterval(handleNextBtn, interval);
-  };
-
-  nextBtn.addEventListener("click", () => {
-    handleNextBtn();
-    if (interval) resetTimer();
-  });
-
-  prevBtn.addEventListener("click", () => {
-    handlePrevBtn();
-    if (interval) resetTimer();
-  });
+  track.addEventListener('mousedown', gestureStart);
+  track.addEventListener('mousemove', gestureMove);
+  track.addEventListener('mouseup', gestureEnd);
 };
 
-carousel({
-  prevBtn: document.querySelector(".slider__prev-btn"),
-  nextBtn: document.querySelector(".slider__next-btn"),
-  container: document.querySelector(".slider__container"),
-  items: document.querySelectorAll(".slider__item"),
+window.addEventListener('load', () => {
+  slider();
 });
 
-carousel({
-  prevBtn: document.querySelector(".testimonials__prev-btn"),
-  nextBtn: document.querySelector(".testimonials__next-btn"),
-  container: document.querySelector(".testimonials__carousel-container"),
-  items: document.querySelectorAll(".testimonials__text"),
-  interval: 5000,
-});
-
-document.querySelector(".navbar-search__btn").addEventListener("click", () => {
+document.querySelector('.navbar-search__btn').addEventListener('click', () => {
   document
-    .querySelector(".navbar-search__input-wrapper")
-    .classList.toggle("navbar-search__input-wrapper--open");
-  document.querySelector(".navbar-search__input").focus();
+    .querySelector('.navbar-search__input-wrapper')
+    .classList.toggle('navbar-search__input-wrapper--open');
+  document.querySelector('.navbar-search__input').focus();
 });
 
-document.body.addEventListener("click", (e) => {
-  if (!e.target.closest(".navbar-search")) {
+document.body.addEventListener('click', (e) => {
+  if (!e.target.closest('.navbar-search')) {
     document
-      .querySelector(".navbar-search__input-wrapper")
-      .classList.remove("navbar-search__input-wrapper--open");
+      .querySelector('.navbar-search__input-wrapper')
+      .classList.remove('navbar-search__input-wrapper--open');
   }
 });
 
-const navtoggler = document.querySelector(".navbar-toggler");
-const navbar = document.querySelector(".nav");
+const navtoggler = document.querySelector('.navbar-toggler');
+const navbar = document.querySelector('.nav');
 
-navtoggler.addEventListener("click", () => {
-  navbar.classList.toggle("nav--open");
+navtoggler.addEventListener('click', () => {
+  navbar.classList.toggle('nav--open');
 });
